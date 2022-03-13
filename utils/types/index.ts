8a74719f58eaa8f53cad5b38477e1dbbb5e7f914
ThomasFoydel <thomasjfoydel@gmail.com>
2022-03-13 01:06:45 -0600

    added data types

--- utils/types/index.ts
+++ utils/types/index.ts
@@ -1,10 +1,62 @@
-// You can include shared interfaces/types in a separate file
-// and then use them in any component by importing them. For
-// example, to import the interface below do:
-//
-// import { User } from 'path/to/interfaces';
+export interface iUser {
+  id: string
+  commissions: [iCommission]
+  ownEntries: [iEntry]
+  entriesMade: string
+  commissionsCreated: string
+  commissionsCompleted: string
+  commissionsCancelled: string
+  commissionsWon: string
+  votesEarned: string
+  votesClaimed: string
+  votesCast: string
+  tipsEarned: string
+  valueContributed: string
+}
 
-export type User = {
-  id: number
-  name: string
+export interface iVote {
+  id: string
+  contributions: [iContribution]
+  voter: iUser
+  commission: iCommission
+}
+
+export interface iContribution {
+  id: string
+  transactionHashes: [string]
+  author: iUser
+  entry: iEntry
+  total: string
+  vote: iVote
+}
+
+export interface iEntry {
+  id: string
+  contributions: [iContribution]
+  author: iUser
+  voteAmount: string
+  commission: iCommission
+  ipfsPath: string
+  content: string
+  timestamp: string
+}
+
+export interface iCommission {
+  id: string
+  submittedEntries: [iEntry]
+  commissioner: iUser
+  winningAuthor: iUser
+  entryCount: number
+  prompt: string
+  content: string
+  reward: string
+  timestamp: string
+}
+
+export class ErrorResponse {
+  constructor(public message: string, public status: number, public code: number) {}
+}
+
+export class MetaMaskError {
+  constructor(public message: string, public stack: string, public code: number) {}
 }
