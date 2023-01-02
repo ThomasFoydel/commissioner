27a909e2874777aa2c7ba5053b7ecccc41ddab49
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-01 23:39:15 -0500

    corrected goerli chain id in config

--- utils/config.ts
+++ utils/config.ts
@@ -6,14 +6,14 @@ const chains = {
   1: {
     factoryAddress: '0xf825fA580d560817298991523c189fFa4e10A545', // goerli address, need to replace
   },
-  3: {
+  5: {
     factoryAddress: '0xf825fA580d560817298991523c189fFa4e10A545',
   },
 }
 const getConfig = (chainId: number) => {
   const id = chainId || 1
   const chainConfig = chains[id as keyof typeof chains]
-  return chainConfig
+  return chainConfig || chains[0]
 }
 
 export default getConfig
