526e33fc50a2cd365f9817a7e2dd09e5b10886c9
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-04 22:59:17 -0500

    create commission page

---
+++ pages/create/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommissionForm from '../../components/CommissionForm'
+
+const Create = () => {
+  return (
+    <div>
+      <p>CREATE COMMISSION</p>
+      <CommissionForm />
+    </div>
+  )
+}
+
+export default Create
