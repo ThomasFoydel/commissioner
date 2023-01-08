c5ca7b6c09bd13043d452c76146bbf4798a690ef
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-08 17:39:45 -0500

    header styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useEthers } from '@usedapp/core'
 
 const Header = () => {
   const { account } = useEthers()
+  const router = useRouter()
+  const routes = [
+    { path: '/about', label: 'about' },
+    { path: '/commissions', label: 'commissions' },
+    { path: `/user/${account?.toLowerCase()}`, label: 'profile', hide: !account },
+    { path: '/terminal', label: 'terminal' },
+  ]
+  
   return (
     <div style={{ textShadow: 'none' }} className="text-center flex justify-center h-[30px]">
-      <Link href="/about">
-        <a className="mx-3">about</a>
-      </Link>
-      <Link href="/commissions">
-        <a className="mx-3">commissions</a>
-      </Link>
-      <Link href="/create">
-        <a className="mx-3">create</a>
-      </Link>
-      {account && (
-        <Link href={`/user/${account.toLowerCase()}`}>
-          <a className="mx-3">profile</a>
-        </Link>
-      )}
-      <Link href="/terminal">
-        <a className="mx-3">terminal</a>
-      </Link>
+      {routes
+        .filter((route) => !route.hide)
+        .map((route) => (
+          <Link href={route.path}>
+            <a className={`mx-3 ${router.asPath.toLowerCase() === route.path ? 'underline' : ''}`}>
+              {route.label}
+            </a>
+          </Link>
+        ))}
     </div>
   )
 }
