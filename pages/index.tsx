f703e965ea35e961ea8fcec1e79585f5d69a151b
ThomasFoydel <thomasjfoydel@gmail.com>
2023-01-08 02:48:47 -0500

    bigger welcome text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { NextPage } from 'next'
 import Typist from 'react-typist'
-import Layout from './layouts/CRT'
 import 'react-typist/dist/Typist.css'
+import Layout from './layouts/CRT'
 
 const Home = () => {
   return (
-    <Typist avgTypingDelay={175} stdTypingDelay={40}>
+    <Typist avgTypingDelay={175} stdTypingDelay={40} className="text-2xl">
       <span>Wake up Neo...</span>
       <Typist.Backspace count={14} delay={2000} />
       <span>The Matrix has you...</span>
