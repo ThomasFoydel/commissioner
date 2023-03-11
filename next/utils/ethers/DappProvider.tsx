faea01fc85456aa4dadf6ca152ca8b3b763a538e
ThomasFoydel <thomasjfoydel@gmail.com>
2023-03-11 15:05:36 -0500

    added infura key to env vars

--- next/utils/ethers/DappProvider.tsx
+++ next/utils/ethers/DappProvider.tsx
@@ -4,8 +4,8 @@ import { ChainId, DAppProvider } from '@usedapp/core'
 const config = {
   readOnlyChainId: ChainId.Goerli,
   readOnlyUrls: {
-    [ChainId.Mainnet]: 'https://mainnet.infura.io/v3/05588c95cd2845e79ecca2237548ecaf',
-    [ChainId.Goerli]: 'https://goerli.infura.io/v3/05588c95cd2845e79ecca2237548ecaf',
+    [ChainId.Mainnet]: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+    [ChainId.Goerli]: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
   },
 }
 
